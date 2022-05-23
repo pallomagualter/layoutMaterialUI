025db8d34201c0c6a40ebcb3da20093b241f40a2
pallomagualter <pallomagualter@gmail.com>
2022-05-23 16:00:34 -0300

    feat: added react router dom in version 6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Olá Mundo!</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
