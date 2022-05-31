a0a0cb4dc05c1cffdf83488f14699136a92d12aa
pallomagualter <pallomagualter@gmail.com>
2022-05-31 17:55:43 -0300

    feat: added in drawer the user avatar and list item menu

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,4 +1,4 @@
-import { Drawer, useTheme } from '@mui/material';
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 
 export const MenuLateral: React.FC = ({ children }) => {
@@ -7,10 +7,34 @@ export const MenuLateral: React.FC = ({ children }) => {
   return (
     <>
       <Drawer variant='permanent' >
-        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
-          {children}
+        <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column' >
+
+          <Box width='100%' height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src='https://avatars.githubusercontent.com/u/33058536?v=4'
+            />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1} >
+            <List component='nav'>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary='Página Inicial' />
+              </ListItemButton>
+            </List>
+          </Box>
+
         </Box>
       </Drawer>
+
+      <Box height="100vh" marginLeft={theme.spacing(28)} >
+        {children}
+      </Box>
     </>
   );
 };
